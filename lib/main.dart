982729edcee4521c5bdeb0b05c3de7c8e6d30c09
olciaciura <olcia.ciura@gmail.com>
2025-05-18 20:48:50 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+  import 'package:flutter/material.dart';
+    import 'package:camera/camera.dart';
+    import 'dart:io';
+    import 'package:flutter/foundation.dart';
+
+    List<CameraDescription> cameras = [];
+
+    void main() async {
+        WidgetsFlutterBinding.ensureInitialized();
+        cameras = await availableCameras();
+        runApp(FlowerApp());
+    }
+
+    class FlowerApp extends StatelessWidget {
+        @override
+        Widget build(BuildContext context) {
+            return MaterialApp(
+                title: 'Flower Application',
+                theme: ThemeData(primarySwatch: Colors.blue),
+                home: CameraScreen(),
+            );
+        }
+    }
+
+    class CameraScreen extends StatefulWidget {
+        @override
+        _CameraScreenState createState() => _CameraScreenState();
+    }
+
+    class _CameraScreenState extends State<CameraScreen> {
+        late CameraController _cameraController;
+        late Future<void> _initializeControllerFuture;
+        File? _capturedPhoto;
+
+        @override
+        void initState() {
+            super.initState();
+            _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+            _initializeControllerFuture = _cameraController.initialize();
+        }
+
+        @override
+        void dispose() {
+            _cameraController.dispose();
+            super.dispose();
+        }
+
+        Future<void> _capturePhoto() async {
+            try {
+                await _initializeControllerFuture;
+                final image = await _cameraController.takePicture();
+                setState(() {
+                    _capturedPhoto = File(image.path);
+                });
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => PhotoPreviewScreen(
+                            photo: _capturedPhoto!,
+                            onSave: _savePhoto,
+                        ),
+                    ),
+                );
+            } catch (e) {
+                print(e);
+            }
+        }
+        void _savePhoto(BuildContext context) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PredictionScreen(),
+              ),
+            );
+        }
+
+        @override
+        Widget build(BuildContext context) {
+            return Scaffold(
+                appBar: AppBar(title: Text('Flower Application')),
+                body: FutureBuilder<void>(
+                    future: _initializeControllerFuture,
+                    builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                            return CameraPreview(_cameraController);
+                        } else {
+                            return Center(child: CircularProgressIndicator());
+                        }
+                    },
+                ),
+                floatingActionButton: FloatingActionButton(
+                    onPressed: _capturePhoto,
+                    child: Icon(Icons.camera),
+                ),
+            );
+        }
+    }
+
+    class PhotoPreviewScreen extends StatelessWidget {
+        final File photo;
+        final Function(BuildContext) onSave;
+
+        PhotoPreviewScreen({required this.photo, required this.onSave});
+
+        @override
+        Widget build(BuildContext context) {
+            return Scaffold(
+                appBar: AppBar(title: Text('Photo Preview')),
+                body: Column(
+                    children: [
+                        Expanded(
+                            child: kIsWeb
+                                ? Image.network(photo.path)
+                                : Image.file(photo),
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                                ElevatedButton(
+                                    onPressed: () {
+                                        Navigator.pop(context);
+                                        onSave(context);
+                                    },
+                                    child: Text('Save'),
+                                ),
+                                ElevatedButton(
+                                    onPressed: () {
+                                        Navigator.pop(context);
+                                    },
+                                    child: Text('Discard'),
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            );
+        }
+    }
+
+    class PredictionScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(title: Text('Prediction')),
+            body: Center(
+                child: Text(
+                    'Predicted Flowers',
+                  
+                    style: TextStyle(fontSize: 24),
+                ),
+            ),
+        );
+    }
+}
